0af89b886a767574fd136141ecba2d5ea74e57c4
XaiNano <itsawesome17@gmail.com>
2016-11-16 17:10:08 +0100

    First commit

---
+++ app/src/main/java/com/example/user/srpanel/CustomAdapter.java
@@ -0,0 +1,43 @@
+package com.example.user.srpanel;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+
+/**
+ * Created by user on 16/11/2016.
+ */
+public class CustomAdapter extends FragmentPagerAdapter {
+    private String fragments[] = {"Analysis", "Algebra", "Geometry"};
+
+    public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+        super(supportFragmentManager);
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Fragment1();
+            case 1:
+                return new Fragment2();
+            case 2:
+                return new Fragment3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragments[position];
+    }
+}
